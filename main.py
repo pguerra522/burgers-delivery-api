511df8ee435d9be38b3dca725ffb1016895c46a3
pguerra522 <pguerra522@gmail.com>
2024-07-28 20:14:59 -0600

    added authentication

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from order_routes import order_router
 from auth_routes import auth_router
+from fastapi_jwt_auth import AuthJWT
+from schemas import Settings
+import inspect, re
+from fastapi import FastAPI
+from fastapi.routing import APIRoute
+from pydantic import BaseModel
+
+@AuthJWT.load_config
+def get_config():
+    return Settings()
 
 app = FastAPI()
 
---
+++ schemas.py
@@ -0,0 +1,52 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class SignUpModel(BaseModel):
+    id:Optional[int]
+    username:str
+    email:str
+    password:str
+    is_staff:Optional[bool]
+    is_active:Optional[bool]
+
+
+    class Config:
+        orm_mode=True
+        schema_extra={
+            'example':{
+                "username":"johndoe",
+                "email":"johndoe@gmail.com",
+                "password":"password",
+                "is_staff":False,
+                "is_active":True
+            }
+        }
+
+
+class Settings(BaseModel):
+    authjwt_secret_key:str='0d1d55b56e58d31ed480a5a8ad088f620d8091a3a958444476c02a5454b00f9c'
+
+class LoginModel(BaseModel):
+    username:str
+    password:str
+
+
+
+
+
+class OrderModel(BaseModel):
+    id:Optional[int]
+    quantity:int
+    order_status:Optional[str]="PENDING"
+    combo_size:Optional[str]="SMALL"
+    user_id:Optional[int]
+
+
+    class Config:
+        orm_mode=True
+        schema_extra={
+            "example":{
+                "quantity":2,
+                "pizza_size":"LARGE"
+            }
+        }
