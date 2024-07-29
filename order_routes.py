8cefa3b7c24dfd891391023bcf55fcc84d23ec71
pguerra522 <pguerra522@gmail.com>
2024-07-28 21:52:16 -0600

    Delete orders endpoint

--- order_routes.py
+++ order_routes.py
@@ -278,3 +278,27 @@ async def update_order_status(id:int,
             }
 
         return jsonable_encoder(response)
+
+
+@order_router.delete('/order/delete/{id}/',status_code=status.HTTP_204_NO_CONTENT)
+async def delete_an_order(id:int,Authorize:AuthJWT=Depends()):
+
+    """
+        ## Delete an Order
+        This deletes an order by its ID
+    """
+
+    try:
+        Authorize.jwt_required()
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid Token")
+
+
+    order_to_delete=session.query(Order).filter(Order.id==id).first()
+
+    session.delete(order_to_delete)
+
+    session.commit()
+
+    return order_to_delete
